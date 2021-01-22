4b0c285f10e6a6b5ef402e1a434eb7c7399a171a
koroshmpr <korosh.mpr@gmail.com>
2021-01-22 17:04:33 +0330

    string Split() , slice() methods

---
+++ web 3-9/3-9.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = 'teal'
+// string slice() method
+let slice = document.getElementById('slice')
+let intro = "Hello World"
+let item = intro.slice(0, 5)
+slice.append(item)
+// end of slice() method
+
+// string split() method
+var str = "How are you doing today?";
+var res = str.split(" ");
+console.log(res)
+// end of split() method
