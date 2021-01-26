1baa96e07bdb8ce9f8274a96e917476ce20adb84
koroshmpr <korosh.mpr@gmail.com>
2021-01-26 20:50:44 +0330

    searchBar

--- web 3-10/3-10.js
+++ web 3-10/3-10.js
@@ -1,23 +1,40 @@
 document.body.style.backgroundColor = 'teal'
 const form = document.getElementById('myform')
 const link = document.getElementById('link')
-const btn = document.getElementById('btn')
 const linkUrl = document.getElementById('linkUrl')
-const asn = document.getElementById('assign')
-const type = document.getElementById('type')
+const src = document.getElementById('search')
+const reload = document.getElementById('reload')
+const sec = document.getElementById('sec')
 linkUrl.setAttribute('href', "linkAddres")
+function none(evvt) {
+    evvt.preventDefault()
+}
 
-form.onsubmit = function (evt) {
+
+function search(evt) {
     evt.preventDefault()
     let linkAddres = link.value
     window.location = linkAddres
 }
-btn.onclick = function (e) {
-    let linkAddres = link.value
-    e.preventDefault()
-    window.location = linkAddres
-}
-type.onclick = function(evtt){
+function ref(evtt) {
     evtt.preventDefault()
-    window.location ='#'
+    location.reload()
 }
+function linker(et) {
+    et.preventDefault()
+    const btn = document.getElementById('bt1')
+    sec.append(btn)
+    btn.addEventListener('click', search)
+    form.addEventListener('submit', search)
+}
+function refresher(ett) {
+    ett.preventDefault()
+    const btn2 = document.getElementById('bt1')
+    sec.append(btn2)
+    btn2.addEventListener('click', ref)
+    form.addEventListener('submit', ref)
+}
+src.addEventListener('click', linker)
+reload.addEventListener('click', refresher)
+form.addEventListener('submit', none)
+bt1.addEventListener('click', none)
