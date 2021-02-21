28663946437d38ce0434af977120c087e463afc4
koroshmpr <korosh.mpr@gmail.com>
2021-02-21 20:06:56 +0330

    todo list with jquery

--- web 3-15/3-15.js
+++ web 3-15/3-15.js
@@ -1,40 +1,36 @@
 //variable defining//
 let i
 let counter = localStorage.length
-const form = $("#myform")
-form.attr('class', 'formse')
-const input = $("#todolist")
-const list = $("#lists")
+const form = $('#myform').addClass('formse')
+const input = $('#todolist')
+const list = $('#lists')
 // create list of localStorages //
 for (i = 0; i < localStorage.length; i++) {
-    $('<li id = "lil" class="task"></li>')
-    $("<button id='btn2' class='cl'>X</button>")
-    let inner = localStorage.getItem(i)
-    $('#lil').innerText = inner
-    $('#list').append($('#lil'))
-    $('#lil').prepend($('#btn2'))
-    close = function (det) {
-        det.preventDefault()
-        $('#btn2').parentElement.remove()
+    let lil = $('<li></li>').addClass('task').text(localStorage.getItem(i))
+    const btn = $('<button></button>').text("X").addClass('cl')
+    close = (e) => {
+        e.preventDefault()
+        btn.parent(this).remove()
     }
-    $('#btn2').click(close)
+    lil.prepend(btn)
+    list.append(lil)
+    btn.click(close)
 }
 //create toDolist //
 const toDoList = function (e) {
     e.preventDefault()
-    localStorage.setItem(counter, input.value)
-    $('<li id="flist" class="task"></li>')
-    $('#flist').innerText = localStorage.getItem(counter)
-    $("<button id='btn' class='cl'>X</button>")
-    counter = counter + 1
-    $('#flist').prepend($('#btn'))
-    $('#list').append($('#flist'))
-    // function for  closing list//
-    close = function (c) {
-        c.preventDefault()
-        $('#btn').parentElement.remove()
+    localStorage.setItem(counter, input.val())
+    let flist = $('<li></li>')
+    flist.addClass('task').text(localStorage.getItem(counter))
+    const btn = $('<button></button>').text("X").addClass('cl')
+    close = (e) => {
+        e.preventDefault()
+        btn.parent(this).remove()
     }
-    $('#btn').click(close)
+    flist.prepend(btn)
+    list.append(flist)
+    counter = counter + 1
+    btn.click(close)
+    this.reset()
 }
-form.submit(toDoList);
-
+form.submit(toDoList)
