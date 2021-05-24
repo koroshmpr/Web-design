7632c08494b72e387b94781d79aeb01c1f1c84e2
koroshmpr <korosh.mpr@gmail.com>
2021-05-24 15:57:56 +0430

    compelete xo game

---
+++ showcase/xo/xo.js
@@ -0,0 +1,38 @@
+let box = $('.cell')
+let reld = document.getElementById('reld')
+// function x() {
+//     $(this).innerText = 'X'
+//     $(this).css('backgroundColor', 'red')
+//     $(this).css('fontSize', '100px')
+// }
+// function o() {
+//     $(this).innerText = 'X'
+//     $(this).css('backgroundColor', 'teal')
+//     $(this).css('fontSize', '100px')
+// }
+function rd(e) {
+    e.preventDefault()
+    location.reload()
+}
+reld.addEventListener('click', rd)
+$('.cell').on("click", function () {
+    if ($(this).attr("mode") === "white" & $(this).attr("Condition") !== "check") {
+        $(this).css('backgroundColor', 'teal'),
+        $(this).css('fontSize' , '100px')
+        this.innerText ='X'
+            $('.cell').attr("mode", "teal"),
+            $(this).attr("Condition", "check")
+    }
+    else if ($(this).attr("mode") === "teal" & $(this).attr("Condition") !== "check") {
+        $(this).css('backgroundColor', 'red'),
+            $('.cell').attr("mode", "white"),
+            $(this).css('fontSize' , '100px')
+        this.innerText ='O'
+            $(this).attr("Condition", "check")
+    }
+    // else if ($(this).attr("mode") === "red") {
+    //     $(this).css('backgroundColor', 'teal'),
+    //         $('.cell').attr("mode", "teal")
+    // }
+
+})
