919ce6f79bd387532575376b809c70cf28e11418
koroshmpr <korosh.mpr@gmail.com>
2021-05-24 12:06:38 +0430

    update shadow

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let info = document.getElementById('info')
 function change() {
     info.classList.toggle("non")
 }
+let wr = document.getElementById('wr')
+function flip() {
+    wr.classList.toggle('flip')
+}
