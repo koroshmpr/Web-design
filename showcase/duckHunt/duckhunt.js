8f43075a9b0519a42ad69cad40b2a90bcead8a7a
koroshmpr <korosh.mpr@gmail.com>
2021-06-02 12:33:55 +0430

    update dk live

--- showcase/duckHunt/duckhunt.js
+++ showcase/duckHunt/duckhunt.js
@@ -6,6 +6,7 @@ let sco = document.getElementById('sco')
 const caught = document.getElementById('caught')
 const ls = document.getElementById('ls')
 const board = document.getElementById('brd')
+const sce = document.getElementById('score')
 sco.append(score)
 window.onload = function () {
     setTimeout(func1, 7000)
@@ -16,6 +17,14 @@ function srd() {
     sco.removeChild(sco.childNodes[1]);
     score += 10
     sco.append(score)
+    if (score === 100 || score === 200 || score === 300) {
+        heart.removeChild(heart.lastChild)
+        sce.play()
+        find--
+        let hb = document.createElement('i');
+        hb.classList = ('fas fa-heart');
+        heart.prepend(hb)
+    }
 }
 $('#dk').click(srd)
 function func1() {
@@ -29,11 +38,11 @@ function change() {
     var randPosY = Math.floor((Math.random() * bodyHeight / 2));
     $('#dk').css('left', randPosX);
     $('#dk').css('top', randPosY);
-    if (randPosX < bodyWidth/2){
+    if (randPosX < bodyWidth / 2) {
         num = 180
     }
-    else if (randPosX> bodyWidth/2) {
-        num =0
+    else if (randPosX > bodyWidth / 2) {
+        num = 0
     }
     $('#dk').css('transform', `rotateY(${num}deg)`)
 }
@@ -43,7 +52,7 @@ setInterval(function () {
 }, 900);
 $('#losing').click(loser)
 function loser() {
-    heart.removeChild(heart.childNodes[0])
+    heart.removeChild(heart.firstChild)
     let ht = document.createElement('i');
     ht.classList = ('fas fa-heart-broken');
     heart.append(ht)
