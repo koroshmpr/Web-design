d38e6215b00970c5c4343954760db2a38734a8d9
koroshmpr <korosh.mpr@gmail.com>
2021-06-13 14:16:19 +0430

    up chess

--- showcase/Chess/chess.js
+++ showcase/Chess/chess.js
@@ -8,29 +8,32 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 function drop(ev) {
-    const mored = ev.target.firstChild.getAttribute('id')
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(ev.target.firstChild.getAttribute('id'))
-    if (mored  == 'sarbc' || mored == 'sarba' ||
-     mored == 'sarbb' || mored == 'sarbd' || mored == 'sarbe' ||
+    if (ev.target.firstChild != null) {
+        const mored = ev.target.firstChild.getAttribute('id')
+        if (mored  == 'sarbc' || mored == 'sarba' ||
+    mored == 'sarbb' || mored == 'sarbd' || mored == 'sarbe' ||
      mored == 'sarbf' || mored == 'sarbg' || mored == 'sarbh' ||
-      mored == 'rokhbl' || mored == 'rokhbr' || 
-      mored == 'asbbl' || mored == 'asbbr' || 
-      mored == 'filbl' || mored == 'filbr' || 
-      mored == 'qb'){
+     mored == 'rokhbl' || mored == 'rokhbr' || 
+     mored == 'asbbl' || mored == 'asbbr' || 
+     mored == 'filbl' || mored == 'filbr' || 
+     mored == 'qb'){
         blackls.append(ev.target.firstChild)
     }
     else if (mored  == 'sarwc' || mored == 'sarwa' ||
-     mored == 'sarwb' || mored == 'sarwd' || mored == 'sarwe' ||
+    mored == 'sarwb' || mored == 'sarwd' || mored == 'sarwe' ||
      mored == 'sarwf' || mored == 'sarwg' || mored == 'sarwh' ||
-      mored == 'rokhwl' || mored == 'rokhwr' || 
+     mored == 'rokhwl' || mored == 'rokhwr' || 
       mored == 'asbwl' || mored == 'asbwr' || 
       mored == 'filwl' || mored == 'filwr' || 
       mored == 'qw'){
-        whitels.append(ev.target.firstChild)
+          whitels.append(ev.target.firstChild)
+        }
     }
-    ev.target.appendChild(document.getElementById(data));
+   
+        ev.target.appendChild(document.getElementById(data));
+    
 }
 $('.black').attr('ondrop', 'drop(event)')
 $('.black').attr("ondragover", "allowDrop(event)")
