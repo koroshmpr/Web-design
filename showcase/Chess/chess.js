5b8df783a87ebe5bd9c60c1a17e6b1bd518990be
koroshmpr <korosh.mpr@gmail.com>
2021-06-13 13:22:41 +0430

    add chess game

---
+++ showcase/Chess/chess.js
@@ -0,0 +1,42 @@
+const blackls = document.getElementById('loserb')
+const whitels = document.getElementById('loserw')
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev) {
+    const mored = ev.target.firstChild.getAttribute('id')
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    console.log(ev.target.firstChild.getAttribute('id'))
+    if (mored  == 'sarbc' || mored == 'sarba' ||
+     mored == 'sarbb' || mored == 'sarbd' || mored == 'sarbe' ||
+     mored == 'sarbf' || mored == 'sarbg' || mored == 'sarbh' ||
+      mored == 'rokhbl' || mored == 'rokhbr' || 
+      mored == 'asbbl' || mored == 'asbbr' || 
+      mored == 'filbl' || mored == 'filbr' || 
+      mored == 'qb'){
+        blackls.append(ev.target.firstChild)
+    }
+    else if (mored  == 'sarwc' || mored == 'sarwa' ||
+     mored == 'sarwb' || mored == 'sarwd' || mored == 'sarwe' ||
+     mored == 'sarwf' || mored == 'sarwg' || mored == 'sarwh' ||
+      mored == 'rokhwl' || mored == 'rokhwr' || 
+      mored == 'asbwl' || mored == 'asbwr' || 
+      mored == 'filwl' || mored == 'filwr' || 
+      mored == 'qw'){
+        whitels.append(ev.target.firstChild)
+    }
+    ev.target.appendChild(document.getElementById(data));
+}
+$('.black').attr('ondrop', 'drop(event)')
+$('.black').attr("ondragover", "allowDrop(event)")
+$('.white').attr('ondrop', 'drop(event)')
+$('.white').attr("ondragover", "allowDrop(event)")
+$('.p1').attr('draggable', 'true')
+$('.p1').attr('ondragstart', 'drag(event)')
+$('.p2').attr('draggable', 'true')
+$('.p2').attr('ondragstart', 'drag(event)')
